26ea80e2984308173b34ce1115d2e865ec1bb93e
Lúcás Meier <cronokirby@gmail.com>
2020-06-06 14:07:39 +0200

    Create a basic example to demonstrate the signalling server
    This creates some inputs to allow us to send a message to another user,
    and we stream the messages we receive and display them as well.
    
    The next steps are to actually send useful messages to get WebRTC going!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,61 @@
 import Head from 'next/head';
+import React from 'react';
+import Signaller from '../src/frontend/Signaller';
+import type { ID } from '../src/identifier';
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Mooz | Cronokirby</title>
-    </Head>
-    <main>
-      <h2>Welcome!</h2>
-      <p>This is just a blank page for now</p>
-    </main>
-  </div>
-);
+function Messages(props: { messages: any[] }) {
+  return (
+    <ul>
+      {props.messages.map((x, i) => (
+        <li key={i}>{JSON.stringify(x)}</li>
+      ))}
+    </ul>
+  );
+}
 
-export default Home;
+function Home() {
+  const [messages, setMessages] = React.useState<any[]>([]);
+  const [to, setTo] = React.useState('');
+  const [text, setText] = React.useState('');
+  const { current: signaller } = React.useRef(new Signaller());
+
+  const sendMessage = () => {
+    signaller.send({ data: text }, to as ID);
+  };
+
+  const listenToMessages = async () => {
+    for await (const message of signaller) {
+      setMessages((ms) => [...ms, message]);
+    }
+  };
+
+  React.useEffect(() => {
+    listenToMessages();
+  }, []);
+
+  return (
+    <>
+      <h2>Your ID:</h2>
+      <p>{signaller.id}</p>
+      <h2>Send a Message To:</h2>
+      <input onChange={(e) => setTo(e.target.value)}></input>
+      <textarea onChange={(e) => setText(e.target.value)}></textarea>
+      <button onClick={sendMessage}>Send</button>
+      <h2>Your Messages:</h2>
+      <Messages messages={messages} />
+    </>
+  );
+}
+
+export default function HomePage() {
+  return (
+    <div>
+      <Head>
+        <title>Mooz | Cronokirby</title>
+      </Head>
+      <main>
+        <Home />
+      </main>
+    </div>
+  );
+}
