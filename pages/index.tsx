61b002ce3da7e7b68059877e4449a80da20dfb84
Lúcás Meier <cronokirby@gmail.com>
2020-06-06 10:42:19 +0200

    Setup typescript
    Even though this is probably not going to be a large project, I want to
    have typescript anyways.

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Mooz | Cronokirby</title>
+    </Head>
+    <main>
+      <h2>Welcome!</h2>
+      <p>This is just a blank page for now</p>
+    </main>
+  </div>
+);
+
+export default Home;
