8aae4dca98c4dca740dc6c03c2892ef90e2fa14b
Lúcás Meier <cronokirby@gmail.com>
2020-06-08 21:15:50 +0200

    Add share button

--- pages/room/[id].tsx
+++ pages/room/[id].tsx
@@ -1,9 +1,48 @@
-import { useRouter } from 'next/router';
+import { NextPageContext } from 'next';
 import React from 'react';
+import * as icons from '../../components/icons';
 import { listen, makeCall } from '../../src/frontend/calling';
 import Signaller from '../../src/frontend/Signaller';
 import type { ID } from '../../src/identifier';
-import { NextPageContext } from 'next';
+
+interface ButtonProps {
+  onClick(): void;
+}
+
+function Button(props: React.PropsWithChildren<ButtonProps>) {
+  return (
+    <button
+      className="p-2 rounded-full bg-main-700 hover:bg-main-500 transition-colors duration-500 shadow-lg text-white"
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
+}
+
+function ShareButton({ id }: { id: ID }) {
+  const url = `https://mooz.cronokirby.now.sh/room/${id}`;
+  const title = `Mooz meeting ${id}`;
+
+  const share = () => {
+    const nav = navigator as any;
+    if (nav.share) {
+      nav.share({ title: title, url });
+    } else {
+      navigator.clipboard.writeText(url);
+    }
+  };
+
+  return <Button onClick={share}>{icons.Share(32)}</Button>;
+}
+
+function Buttons({ id }: { id: ID }) {
+  return (
+    <div className="absolute left-0 bottom-0 p-8 md:p-16 flex items-col justify-between space-y-4">
+      <ShareButton id={id} />
+    </div>
+  );
+}
 
 function Room(props: { signaller: Signaller; call?: ID }) {
   const myVideoRef = React.useRef(null as HTMLVideoElement | null);
@@ -49,7 +88,7 @@ function Room(props: { signaller: Signaller; call?: ID }) {
       // For older browsers
       (video as any).src = window.URL.createObjectURL(remoteStream);
     }
-    setConnected(true)
+    setConnected(true);
   };
 
   React.useEffect(() => {
@@ -57,8 +96,18 @@ function Room(props: { signaller: Signaller; call?: ID }) {
   }, [myStream, theirVideoRef]);
 
   return (
-    <div className={`transition-colors duration-500 w-full h-screen relative ${connected ? 'bg-gray-900' : 'bg-main-100'}`}>
-      <video autoPlay playsInline controls={false} ref={theirVideoRef} className="w-full h-full"></video>
+    <div
+      className={`transition-colors duration-500 w-full h-screen relative ${
+        connected ? 'bg-gray-900' : 'bg-main-100'
+      }`}
+    >
+      <video
+        autoPlay
+        playsInline
+        controls={false}
+        ref={theirVideoRef}
+        className="w-full h-full"
+      ></video>
       <div className="rounded-md shadow-lg mt-8 mr-8 mb-8 w-32 sm:w-48 lg:w-64 absolute right-0 top-0 sm:top-auto sm:bottom-0">
         <video
           className="rounded-md w-full"
@@ -69,6 +118,7 @@ function Room(props: { signaller: Signaller; call?: ID }) {
           muted
         ></video>
       </div>
+      <Buttons id={props.call ?? props.signaller.id} />
     </div>
   );
 }
