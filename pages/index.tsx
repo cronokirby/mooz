0ef2cad6a03489a6e38cdc25899ac01b27063e91
Lúcás Meier <cronokirby@gmail.com>
2020-06-08 19:52:17 +0200

    Add a nicer create page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,9 @@
 import Head from 'next/head';
-import React from 'react';
-import { listen, makeCall } from '../src/frontend/calling';
-import Signaller from '../src/frontend/Signaller';
-import { ID, newID } from '../src/identifier';
 import Router from 'next/router';
+import React from 'react';
+import { newID } from '../src/identifier';
 
 function Home() {
-  const [inp, setInp] = React.useState('');
-
   const createMeeting = () => {
     const id = newID();
     Router.push(
@@ -19,18 +15,20 @@ function Home() {
     );
   };
 
-  const joinMeeting = () => {
-    Router.push('/room/[id]', `/room/${inp}`);
-  };
-
   return (
     <>
-      <button onClick={createMeeting}>Create Meeting</button>
-      <input
-        onChange={(e) => setInp(e.target.value)}
-        placeholder="meeting ID"
-      ></input>
-      <button onClick={joinMeeting}>Join Meeting</button>
+      <div className="container mx-auto">
+        <div className="mx-auto flex flex-col items-center w-11/12 sm:w-8/12 md:w-1/2 lg:w-1/3 bg-white rounded shadow-lg rounded-md my-8 py-4">
+          <img className="w-full p-2" src="/bunnyhat.svg">
+          </img>
+          <button
+            onClick={createMeeting}
+            className="bg-main-700 transition-colors duration-500 hover:bg-main-500 shadow-md rounded-full focus:shadow text-2xl text-white font-bold px-4 py-2"
+          >
+            Create Meeting
+          </button>
+        </div>
+      </div>
     </>
   );
 }
@@ -41,7 +39,7 @@ export default function HomePage() {
       <Head>
         <title>Mooz | Cronokirby</title>
       </Head>
-      <main>
+      <main className="w-full">
         <Home />
       </main>
     </div>
